dc6bc489bb633dcb61f8c97a93a1c05638fa336a
TristanDCP <tristandacostapinto@gmail.com>
2021-12-15 14:03:35 +0100

    Second commit outline & title done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react'
 import './App.css';
 
 const App = () => {
-    return <div id="temp-style"> Develop your test here </div>
+    return <div id="temp-style"> 
+        <div class="card-outline">
+            <div class="card-title">
+                <div class="title-text">
+                    PROFIL
+                </div>
+            </div>
+            <div class="card-content" >
+                    Contenu de la card ici
+            </div>
+            
+        </div>
+     </div>
 }
 
 export default App;
